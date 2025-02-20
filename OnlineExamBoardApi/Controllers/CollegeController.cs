1b7fa7095a5919b14804bdb3e51b2526c5cc4d3f
shubham <shubhamchoudhary.ilw@gmail.com>
2025-02-20 14:27:47 +0530

    Add project files.

---
+++ OnlineExamBoardApi/Controllers/CollegeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineExamBoardApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OnlineExamBoardApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CollegeController : ControllerBase
+    {
+        private readonly DbOnlineExamBoardContext _context;
+        public CollegeController(DbOnlineExamBoardContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<CollegeController>
+        [HttpGet]
+        public IEnumerable<TblCollegeDetail> Get()
+        {
+            return  _context.TblCollegeDetails.ToList();
+        }
+
+        // GET api/<CollegeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CollegeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CollegeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CollegeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ OnlineExamBoardApi/Models/TblCollegeDetail.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.EntityFrameworkCore;
+using OnlineExamBoardApi.Models;
+
+namespace OnlineExamBoardApi.Models;
+
+public partial class TblCollegeDetail
+{
+    public int IntId { get; set; }
+
+    public string? StrCollegeName { get; set; }
+
+    public string? StrCollegeAddress { get; set; }
+
+    public string? StrAffiliationNumber { get; set; }
+
+    public string? StrContactNo { get; set; }
+
+    public string? StrEmail { get; set; }
+
+    public string? StrPhoneNo { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    public int? IntCreatedBy { get; set; }
+
+    public DateTime? DteCreatedDate { get; set; }
+
+    public int? IntModifiedBy { get; set; }
+
+    public DateTime? DteModifiedDate { get; set; }
+
+    public virtual TblUser? IntCreatedByNavigation { get; set; }
+
+    public virtual TblUser? IntModifiedByNavigation { get; set; }
+
+    public virtual ICollection<TblExamResult> TblExamResults { get; set; } = new List<TblExamResult>();
+
+    public virtual ICollection<TblStudentDetail> TblStudentDetails { get; set; } = new List<TblStudentDetail>();
+}
+
+
+public static class TblCollegeDetailEndpoints
+{
+	public static void MapTblCollegeDetailEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/TblCollegeDetail").WithTags(nameof(TblCollegeDetail));
+
+        group.MapGet("/", async (DbOnlineExamBoardContext db) =>
+        {
+            return await db.TblCollegeDetails.ToListAsync();
+        })
+        .WithName("GetAllTblCollegeDetails")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", async Task<Results<Ok<TblCollegeDetail>, NotFound>> (int intid, DbOnlineExamBoardContext db) =>
+        {
+            return await db.TblCollegeDetails.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.IntId == intid)
+                is TblCollegeDetail model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetTblCollegeDetailById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int intid, TblCollegeDetail tblCollegeDetail, DbOnlineExamBoardContext db) =>
+        {
+            var affected = await db.TblCollegeDetails
+                .Where(model => model.IntId == intid)
+                .ExecuteUpdateAsync(setters => setters
+                  .SetProperty(m => m.IntId, tblCollegeDetail.IntId)
+                  .SetProperty(m => m.StrCollegeName, tblCollegeDetail.StrCollegeName)
+                  .SetProperty(m => m.StrCollegeAddress, tblCollegeDetail.StrCollegeAddress)
+                  .SetProperty(m => m.StrAffiliationNumber, tblCollegeDetail.StrAffiliationNumber)
+                  .SetProperty(m => m.StrContactNo, tblCollegeDetail.StrContactNo)
+                  .SetProperty(m => m.StrEmail, tblCollegeDetail.StrEmail)
+                  .SetProperty(m => m.StrPhoneNo, tblCollegeDetail.StrPhoneNo)
+                  .SetProperty(m => m.IsActive, tblCollegeDetail.IsActive)
+                  .SetProperty(m => m.IntCreatedBy, tblCollegeDetail.IntCreatedBy)
+                  .SetProperty(m => m.DteCreatedDate, tblCollegeDetail.DteCreatedDate)
+                  .SetProperty(m => m.IntModifiedBy, tblCollegeDetail.IntModifiedBy)
+                  .SetProperty(m => m.DteModifiedDate, tblCollegeDetail.DteModifiedDate)
+                  );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateTblCollegeDetail")
+        .WithOpenApi();
+
+        group.MapPost("/", async (TblCollegeDetail tblCollegeDetail, DbOnlineExamBoardContext db) =>
+        {
+            db.TblCollegeDetails.Add(tblCollegeDetail);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/TblCollegeDetail/{tblCollegeDetail.IntId}",tblCollegeDetail);
+        })
+        .WithName("CreateTblCollegeDetail")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int intid, DbOnlineExamBoardContext db) =>
+        {
+            var affected = await db.TblCollegeDetails
+                .Where(model => model.IntId == intid)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteTblCollegeDetail")
+        .WithOpenApi();
+    }
+}
